aa5da235521bb317a0b70aed1cc316e6a6b80c2c
Francisco Tomas Guzman Fernandez <francistomas96@gmail.com>
2021-10-22 14:35:40 +0200

    first commit

---
+++ src/main/java/com/nttdata/bootcamp/retofuncional/RetoFuncionalApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.retofuncional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoFuncionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoFuncionalApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/nttdata/bootcamp/retofuncional/repository/Aula.java
@@ -0,0 +1,20 @@
+package com.nttdata.bootcamp.retofuncional.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Aula {
+	private List<Usuario> usuarioList;
+
+	public Aula() {
+		this.usuarioList = new ArrayList<>();
+	}
+
+	public List<Usuario> getUsuarioList() {
+		return usuarioList;
+	}
+
+	public void setUsuarioList(List<Usuario> usuarioList) {
+		this.usuarioList = usuarioList;
+	}
+}
